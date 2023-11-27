fbadad722e00183988586827f22aac57696dbf54
Katarzyna <katarzynawolna@Katherines-MacBook.local>
2023-11-27 12:22:42 +0100

    Dodano zadania

--- zadania_30_10_2023_py.py
+++ zadania_30_10_2023_py.py
@@ -9,6 +9,7 @@ Original file is located at
 Zad.1
 """
 
+
 def wyswietl_imiona(imiona):
     if len(imiona) != 5:
         print("Proszę podać dokładnie 5 imion.")
@@ -16,45 +17,56 @@ def wyswietl_imiona(imiona):
         for imie in imiona:
             print(imie)
 
-lista_imion = ["Kasia", "Julia", "Dominika", "Tomasz", "Michał"]
+
+lista_imion = list(str)["Kasia", "Julia", "Dominika", "Tomasz", "Michal"]
 wyswietl_imiona(lista_imion)
 
 """Zad.2"""
 
-#Wykorzystując pętle for
-def pomnóż_przez_dwa_for(lista_liczb):
+# Wykorzystując petle for
+
+
+def pomnoz_przez_dwa_for(lista_liczb):
     wynik = []
     for liczba in lista_liczb:
         wynik.append(liczba * 2)
     return wynik
 
+
 lista = [1, 2, 3, 4, 5]
-wynik = pomnóż_przez_dwa_for(lista)
+wynik = pomnoz_przez_dwa_for(lista)
 print(wynik)
 
-# Wykorzystując listę składaną
-def pomnóż_przez_dwa_lista_skladana(lista_liczb):
+# Wykorzystujac liste składana
+
+
+def pomnoz_przez_dwa_lista_skladana(lista_liczb):
     return [liczba * 2 for liczba in lista_liczb]
 
-lista = [1, 2, 3, 4, 5]
-wynik = pomnóż_przez_dwa_lista_skladana(lista)
+
+lista = list(int)[1, 2, 3, 4, 5]
+wynik = pomnoz_przez_dwa_lista_skladana(lista)
 print(wynik)
 
 """Zad.3"""
 
-def wyświetl_parzyste_elementy(lista_liczb):
+
+def wyswietl_parzyste_elementy(lista_liczb):
     for liczba in lista_liczb:
         if liczba % 2 == 0:
             print(liczba)
 
+
 lista = list(range(1, 11))
-wyświetl_parzyste_elementy(lista)
+wyswietl_parzyste_elementy(lista)
 
 """Zad.4"""
 
-def wyświetl_co_drugi_element(lista_liczb):
+
+def wyswietl_co_drugi_element(lista_liczb):
     for i in range(0, len(lista_liczb), 2):
         print(lista_liczb[i])
 
+
 lista = list(range(1, 11))
-wyświetl_co_drugi_element(lista)
+wyswietl_co_drugi_element(lista)
